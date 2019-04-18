fbf514819dda227280cb7a1e49952f5dba14fc87
yusungsamuel <ssyyu@ucdavis.edu>
2019-04-17 20:55:05 -0700

    create server.js fserver.js file. require package and middleware. Define path to scrape from website.

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var logger = require("morgan");
+// var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var PORT = 3000;
+
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// mongoose.connect(MONGODB_URI);
+
+app.get("/scrape", function(req, res){
+    axios.get("https://myanimelist.net/news/tag/new_anime").then(function(response){
+        
+        var $ = cheerio.load(response.data);
+
+        $("div.news-unit").each(function(i, element){
+            var title = $(element).find("p.title").text()
+            console.log(title)
+            var peek = $(element).find("div.text").text()
+        })
+        res.send("SCRAPED")
+    })
+})
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
